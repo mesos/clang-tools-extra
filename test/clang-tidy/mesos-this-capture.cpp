b9bdf71a38fa95a99390fa16432adaba41fe1068
Benjamin Bannier <bbannier@gmail.com>
2017-06-13 21:31:41 +0200

    Revert "[clang-tidy] Extended this-capture check to diagnose process::loop."
    This reverts commit 43c32c87d817f6cfeeb67188be5d7732c7d5b571.

--- test/clang-tidy/mesos-this-capture.cpp
+++ test/clang-tidy/mesos-this-capture.cpp
@@ -12,13 +12,10 @@ struct Future {
 template <typename F>
 Future<Nothing> defer(int /*pid*/, F) { return {}; }
 
-template <typename PID, typename Iterate, typename Body>
-Future<Nothing> loop(const PID &pid, Iterate &&iterate, Body &&body) { return {}; }
 } // namespace process  {
 
 using process::Future;
 using process::defer;
-using process::loop;
 
 struct S {
   Future<Nothing> future() const { return {}; }
@@ -42,23 +39,6 @@ struct S {
     }
 };
 
-struct P {
-  Future<Nothing> future() const { return {}; }
-
-  void f() {
-    // CHECK-MESSAGES: :[[@LINE+1]]:16: warning: callback capturing this should be dispatched/deferred to a specific PID [mesos-this-capture]
-    loop(this, [this]() { (void)this; }, []() {});
-    // CHECK-MESSAGES: :[[@LINE+1]]:25: warning: callback capturing this should be dispatched/deferred to a specific PID [mesos-this-capture]
-    loop(this, []() {}, [this]() { (void)this; });
-
-    auto l = [this]() { (void)this; };
-    // CHECK-MESSAGES: :[[@LINE+1]]:16: warning: callback capturing this should be dispatched/deferred to a specific PID [mesos-this-capture]
-    loop(this, l, []() {});
-    // CHECK-MESSAGES: :[[@LINE+1]]:25: warning: callback capturing this should be dispatched/deferred to a specific PID [mesos-this-capture]
-    loop(this, []() {}, l);
-  }
-};
-
 // Negatives.
 void f() {
   Future<Nothing>().onAny([]() {});
@@ -75,10 +55,3 @@ struct K {
       future.onAny(l);
     }
 };
-
-void g() {
-  K k;
-  loop(k, []() {}, []() {});
-  loop(k, [k]() {}, []() {});
-  loop(k, []() {}, [k]() {});
-}
